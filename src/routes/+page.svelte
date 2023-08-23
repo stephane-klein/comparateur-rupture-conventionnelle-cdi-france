fc38ff0eac9e4a23cdad0fe33bb8374098030370
Stéphane Klein <contact@stephane-klein.info>
2023-08-23 10:56:31 +0200

    Add disclaimer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -155,7 +155,26 @@
             Si vous partagez la page avec les paramètres dans l'URL, ces paramètres seront visibles dans les
             logs du serveur, si vous souhaitez garder secret ces informations, vous pouvez partager avec vos
             amis la version YAML des données, cela vous permet de garder toutes ces informations secrètes.)</small></p>
+
 </div>
+
+<div class="rounded-md bg-yellow-50 p-4 my-8">
+    <div class="flex">
+        <div class="flex-shrink-0">
+            <svg class="h-5 w-5 text-yellow-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 2.495zM10 5a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 5zm0 9a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
+            </svg>
+        </div>
+        <div class="ml-3">
+            <h3 class="text-sm font-medium text-yellow-800">Avertissement</h3>
+            <div class="mt-2 text-sm text-yellow-700">
+                <p>Cette simulation, ces calculs sont indicatifs. Ils ne substituent pas aux décomptes rééels de <a
+                    href="https://www.unedic.org/">Unédic</a> ou <a href="https://www.pole-emploi.fr">Pôle emploi</a> ou de toute autre organisme..</p>
+            </div>
+        </div>
+    </div>
+</div>
+
 <form>
     <div class="space-y-12 sm:space-y-16">
         <div>
